89a92a241198bd1218886c5904f44012e1064310
mautsa,Kudzanai <149285204+Kmautsa@users.noreply.github.com>
2024-09-10 11:39:58 -0400

    Add files via upload

---
+++ Mini Programs/Hello_World.java
@@ -0,0 +1,12 @@
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name ="Kudzanai Mautsa";
+		System.out.println("Hello World!!\n");
+		System.out.println("My name is "+ name);
+		
+	}
+
+}
