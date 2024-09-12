a6bec782dd780b997ff6821fa1d25e511d2e1e89
mautsa,Kudzanai <149285204+Kmautsa@users.noreply.github.com>
2024-09-12 18:50:28 -0400

    Add files via upload

---
+++ Mini Programs/Simple_Calculator.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Simple_Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner kbd = new Scanner(System.in);
+		System.out.println("Welcome to my Simple Calculator !!");
+		System.out.println("Which operation do you want to use today?"+"(+,-,/,*)");
+		System.out.println("{Enter choice}");
+		final String add ="+",subtraction="-",multiply="*",division="/";
+		String choice = kbd.nextLine();
+		switch(choice) {
+		case add:{
+			System.out.println("You chose Addition ");
+			System.out.println("Please enter first number");
+			int num1 =kbd.nextInt();	
+			System.out.println("Please enter second number");
+			int num2 =kbd.nextInt();
+			int num3 =num1+num2;
+			System.out.println("Your Answer is "+num3+".");
+			}
+			break;
+			
+		case subtraction:{
+			System.out.println("You chose Subtraction ");
+			System.out.println("Please enter first number");
+			int num1 =kbd.nextInt();	
+			System.out.println("Please enter second number");
+			int num2 =kbd.nextInt();
+			int num3 =num1-num2;
+			System.out.println("Your Answer is "+num3+".");
+		}
+			break;
+		
+		case multiply:{
+			System.out.println("You chose Multiplication");
+			System.out.println("Please enter first number");
+			int num1 =kbd.nextInt();	
+			System.out.println("Please enter second number");
+			int num2 =kbd.nextInt();
+			int num3 =num1*num2;
+			System.out.println("Your Answer is "+num3+".");
+		}
+			break;
+		
+		case division:{
+			System.out.println("You chose Division");
+			System.out.println("Please enter first number");
+			int num1 =kbd.nextInt();	
+			System.out.println("Please enter second number");
+			int num2 =kbd.nextInt();
+			int num3 =num1/num2;
+			System.out.println("Your Answer is "+num3+".");
+		}
+			break;
+		default:
+			System.out.println("Please make a Choice");
+			
+		}
+	}
+	}
+		
+	
+
