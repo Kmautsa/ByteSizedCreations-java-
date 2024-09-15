fb827be8e378868600fd055c05d953ede0f0625f
mautsa,Kudzanai <149285204+Kmautsa@users.noreply.github.com>
2024-09-14 23:52:30 -0400

    Add files via upload

---
+++ Mini Programs/CaseManagerForStrings.java
@@ -0,0 +1,14 @@
+// A simple Java Program
+
+import java.util.Scanner;
+
+public class CaseManagerForStrings {
+	
+public static void main(String[]Args) {
+	System.out.println("Welcome to the simple Case manager for Text.");
+	String message1="Java i great fun";
+	String uppercase = message1.toUpperCase();
+	String lowercase = message1.toLowerCase();
+	System.out.println(uppercase +"\n"+ lowercase);
+}
+}
