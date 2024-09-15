157fc46740af688e45a083ba5ceddc3a34bc7a4b
mautsa,Kudzanai <149285204+Kmautsa@users.noreply.github.com>
2024-09-14 23:55:01 -0400

    Update CaseManagerForStrings.java

--- Mini Programs/CaseManagerForStrings.java
+++ Mini Programs/CaseManagerForStrings.java
@@ -6,6 +6,9 @@ public class CaseManagerForStrings {
 	
 public static void main(String[]Args) {
 	System.out.println("Welcome to the simple Case manager for Text.");
+	Scanner kbd = new Scanner(Sytstem.in);
+	System.out.println("Do you want to make text UpperCase(1) or LowerCase(2)");
+	
 	String message1="Java i great fun";
 	String uppercase = message1.toUpperCase();
 	String lowercase = message1.toLowerCase();
