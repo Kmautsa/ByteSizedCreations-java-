00624bdbe626e6eb0b6bf675fd1daddd3589d1b6
mautsa,Kudzanai <149285204+Kmautsa@users.noreply.github.com>
2024-09-17 23:19:01 -0400

    Update WaterIntakeCalculator.java

--- Mini Programs/WaterIntakeCalculator.java
+++ Mini Programs/WaterIntakeCalculator.java
@@ -5,8 +5,9 @@
 	    public static void main(String[] args) {
 	        final int OUNCES_PER_GLASS = 8;
 	        final double ML_PER_OUNCE = 29.5735;
-
+		    //new scanner object for user input
 	        Scanner scanner = new Scanner(System.in);
+		    //prmpt message of greeting and to prompt user
 		System.out.print("Welcome to my Water Intake Calculator!!!");
 	        System.out.print("Enter the number of water glasses: ");
 	        int glasses = scanner.nextInt();
