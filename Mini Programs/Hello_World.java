0590516bac48bfdc136f900cd532f5ae88d315ec
mautsa,Kudzanai <149285204+Kmautsa@users.noreply.github.com>
2024-09-17 21:55:36 -0400

    Update Hello_World.java

--- Mini Programs/Hello_World.java
+++ Mini Programs/Hello_World.java
@@ -5,6 +5,7 @@ public class Hello_World {
 		//declaring the string variable
 		
 		String name ="Kudzanai Mautsa";
+		//printing out data using variable
 		System.out.println("Hello World!!\n");
 		System.out.println("My name is "+ name);
 		System.out.println("I am a student in Computer Science who is aspiring to be a Software Engineer");
